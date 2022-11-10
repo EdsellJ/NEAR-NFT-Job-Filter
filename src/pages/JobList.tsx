7b13d61e7af7f56fb98426b3a2d80a22083c0c56
Lucas Wong <wm111898@gmail.com>
2022-11-10 15:59:11 -0500

    add filter-with-wallet button

--- src/pages/JobList.tsx
+++ src/pages/JobList.tsx
@@ -9,7 +9,10 @@ const JobList = () => {
 				<p className='text-gradient mt-20 mx-auto text-8xl font-semibold text-center md:text-5xl xs:text-3xl'>
 					Your future is NEAR
 				</p>
-				<button className='mx-auto my-8 primary'>Post a job</button>
+				<div className='flex gap-x-3 mx-auto my-8'>
+					<button className='primary'>Post a job</button>
+					<button className='primary'>Filter With Wallet</button>
+				</div>
 				<div className='grid gap-4'>
 					{jobs.map((job: Record<any, any>, index: number) => (
 						<JobListItem job={job} key={`job-${index}`} />
