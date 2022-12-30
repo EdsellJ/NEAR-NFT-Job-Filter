2f52b76e8f84e0f21b266881968c7b4a6426bf12
BisiDavi <oludavidconnect@gmail.com>
2022-12-30 05:16:29 +0100

    job board updated

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,6 +1,6 @@
 export default function Filter() {
 	return (
-		<aside className="w-2/6 mt-20 bg-white p-8 px-5 border border-gray h-96 rounded-xl">
+		<aside className="w-4/12 mt-20 bg-white p-8 px-5 border border-gray h-96 rounded-xl">
 			<h4>Filter</h4>
 		</aside>
 	);
