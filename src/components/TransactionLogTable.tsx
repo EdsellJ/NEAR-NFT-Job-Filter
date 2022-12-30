38878bd5c802a439115039cde0cc38f4c529fb5d
BisiDavi <oludavidconnect@gmail.com>
2022-12-30 03:23:36 +0100

    modal added

--- src/components/TransactionLogTable.tsx
+++ src/components/TransactionLogTable.tsx
@@ -1,12 +1,18 @@
 import transactionLogs from "json/transactionLogs.json";
 
-export default function TransactionLogTable() {
+interface Props {
+	toggleModal: () => void;
+}
+
+export default function TransactionLogTable({ toggleModal }: Props) {
 	return (
 		<div className="container mt-8">
 			<div className="logs mx-auto  w-3/4">
 				<div className="transaction flex items-center space-x-2 mb-4">
 					<h6>Transactions log </h6>{" "}
-					<img src="/images/info_icon.png" alt="info" className="w-3" />
+					<button onClick={toggleModal}>
+						<img src="/images/info_icon.png" alt="info" className="w-3" />
+					</button>
 				</div>
 				<table className="w-full">
 					<thead className="py-4 bg-deep-blue text-white border border-black">
--- src/pages/BadgeCreator.tsx
+++ src/pages/BadgeCreator.tsx
@@ -1,12 +1,20 @@
 import NFTHeroBanner from "components/NFTHeroBanner";
 import TransactionLogTable from "components/TransactionLogTable";
 import NFTLayout from "layouts/NFTLayout";
+import Modal from "components/Modal";
+import { useState } from "react";
 
 export default function BadgeCreator() {
+	const [modal, setModal] = useState(false);
 	return (
-		<NFTLayout title="Mint NFT's">
-			<NFTHeroBanner />
-			<TransactionLogTable />
-		</NFTLayout>
+		<>
+			<Modal modal={modal} modalHandler={() => setModal(false)}>
+				<h4 className="text-center flex mx-auto">I am a modal</h4>
+			</Modal>
+			<NFTLayout title="Mint NFT's">
+				<NFTHeroBanner />
+				<TransactionLogTable toggleModal={() => setModal(true)} />
+			</NFTLayout>
+		</>
 	);
 }
