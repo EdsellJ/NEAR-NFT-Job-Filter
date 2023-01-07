f7c15095a73f119e6ed566e7bea60ee50e3afa4f
BisiDavi <oludavidconnect@gmail.com>
2023-01-07 18:44:58 +0100

    upcoming event udpated

--- src/pages/BadgeCreator.tsx
+++ src/pages/BadgeCreator.tsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
-
+// import { useState } from "react";
+//
 import NFTHeroBanner from "components/NFTHeroBanner";
-import TransactionLogTable from "components/TransactionLogTable";
+// import TransactionLogTable from "components/TransactionLogTable";
 import NFTLayout from "layouts/NFTLayout";
-import Modal from "components/Modal";
+// import Modal from "components/Modal";
 
 export default function BadgeCreator() {
-	const [modal, setModal] = useState(false);
+	// const [modal, setModal] = useState(false);
 	return (
 		<>
-			<Modal modal={modal} modalHandler={() => setModal(false)}>
+			{/* <Modal modal={modal} modalHandler={() => setModal(false)}>
 				<h4 className="text-center flex mx-auto">I am a modal</h4>
-			</Modal>
+			</Modal> */}
 			<NFTLayout title="Mint NFT's">
 				<NFTHeroBanner />
-				<TransactionLogTable toggleModal={() => setModal(true)} />
+				{/* <TransactionLogTable toggleModal={() => setModal(true)} /> */}
+				<img
+					src="/images/upcoming_events.png"
+					alt="upcoming events"
+					className="mx-auto my-8"
+				/>
 			</NFTLayout>
 		</>
 	);
