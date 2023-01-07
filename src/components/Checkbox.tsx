125649df1305dfec4e65d15c3de2425ecb9bcae6
BisiDavi <oludavidconnect@gmail.com>
2023-01-07 04:51:16 +0100

    CheckboxGroup added

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,11 +1,12 @@
 interface Props {
 	label: string;
+	name: string;
 }
 
-export default function Checkbox({ label }: Props) {
+export default function Checkbox({ label, name }: Props) {
 	return (
 		<div className="flex items-center text-deep-blue my-4">
-			<input type="checkbox" className="mr-4" />
+			<input type="checkbox" name={name} className="mr-4" />
 			<label>{label}</label>
 		</div>
 	);
