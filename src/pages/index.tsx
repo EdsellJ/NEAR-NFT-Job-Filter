4435a426565df4ec4f22efe56bc7d3a4dce05afe
BisiDavi <oludavidconnect@gmail.com>
2023-01-11 12:59:24 +0100

    lms page updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,23 +7,37 @@ export default function HomePage() {
 	return (
 		<NFTLayout title="Welcome, starting learning, earning NFTs | EduCoin">
 			<section className="w-full bg-primary">
-				<div className="hero-banner container py-20">
-					<div className="left text-white">
-						<h2 className="my-2 font-lora font-bold">
-							A Learning Management <br /> System for a Decentralized <br />
-							World
-						</h2>
-						<p className="w-1/2 mt-6 font-dmsans">
-							<span className="font-bold">EduCoin</span> is a transformational way to
-							register for classes, sell content, and earn credentials via NFTs.
-						</p>
-						<Button
-							className="bg-white font-dmsans rounded-full text-primary px-6 my-10 border py-2 hover:border-white hover-bg-light-blue"
-							text="Start Now"
-						/>
+				<div className="container mx-auto">
+					<div className="hero-banner flex justify-between py-20">
+						<div className="left text-white w-1/2">
+							<h2 className="my-2 font-lora font-bold">
+								A Learning Management <br /> System for a Decentralized <br />
+								World
+							</h2>
+							<p className="w-1/2 mt-6 font-dmsans">
+								<span className="font-bold">EduCoin</span> is a transformational way to
+								register for classes, sell content, and earn credentials via NFTs.
+							</p>
+							<Button
+								className="bg-white font-dmsans rounded-full text-primary px-6 my-10 border py-2 hover:border-white hover-bg-light-blue"
+								text="Start Now"
+							/>
+						</div>
+						<div className="right images flex flex-col w-1/2 ">
+							<img
+								src="/images/banner-icon-2.svg"
+								className="w-1/2 ml-60"
+								alt="banner-2"
+							/>
+							<img
+								src="/images/banner-icon.svg"
+								className="w-1/2 -mt-14 ml-14"
+								alt="banner"
+							/>
+						</div>
 					</div>
-					<div className="right images"></div>
 				</div>
+				<div className="h-8 border-banner-gradient w-full"></div>
 				<div className="content bg-white py-8">
 					<div className="container">
 						<h2 className="mt-4 mb-2 font-lora text-primary ">
